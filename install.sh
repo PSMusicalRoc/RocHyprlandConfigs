5375e460d8306d3f47ca954bdeadb3d658fe3ad9
DumbAsARoc <timbishop514@gmail.com>
2023-11-25 17:54:52 -0500

    added missing .local folder

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 echo "Installing configuration folders!"
 rsync -aP ./.config/ $HOME/.config/
+rsync -aP ./local/ $HOME/.local/
 rsync -aP ./.hyprland_scripts/ $HOME/.hyprland_scripts/
 echo "All done!"
